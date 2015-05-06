8d156da6195b364fad41ccddcdb5d1dfce6a64fa
Tristan Rubadeau <tristan@rubadeau.com>
2015-05-06 14:10:28 -0600

    updated auto size flag

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 	$('.dark').on('click', function() {
-		$('body').css('background-image', 'url(img/dark_wood.png)',
-		 "background-size", "auto");
+		$('body').css({'background-image': 'url(img/dark_wood.png)',
+		 "background-size": "auto"});
 	});
 
 	$('.light').on('click', function() {
-		$('body').css('background-image', 'url(img/tileable_wood_texture.png)',
-		 "background-size", "auto");
+		$('body').css({'background-image': 'url(img/tileable_wood_texture.png)',
+		 "background-size": "auto"});
 	});
 
 	$('.darker').on('click', function() {
-		$('body').css('background-image', 'url(img/forest.jpg)',
-		 "background-size", "cover");
+		$('body').css({'background-image': 'url(img/forest.jpg)',
+		 "background-size": "cover"});
 	});
 
 	$(".high").on("click", function() {
