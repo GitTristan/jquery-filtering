31b627d667aaa64d0ef0bd52498bae2258f692bd
Tristan Rubadeau <tristan@rubadeau.com>
2015-05-06 10:58:54 -0600

    updated backgrounds to auto size

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
 	$('.dark').on('click', function() {
-		$('body').css('background-image', 'url(img/dark_wood.png)');
+		$('body').css('background-image', 'url(img/dark_wood.png)',
+		 "background-size", "auto");
 	});
 
 	$('.light').on('click', function() {
-		$('body').css('background-image', 'url(img/tileable_wood_texture.png)');
+		$('body').css('background-image', 'url(img/tileable_wood_texture.png)',
+		 "background-size", "auto");
 	});
 
 	$('.darker').on('click', function() {
-		$('body').css('background-image', 'url(img/forest.jpg)');
+		$('body').css('background-image', 'url(img/forest.jpg)',
+		 "background-size", "cover");
 	});
 
 })
